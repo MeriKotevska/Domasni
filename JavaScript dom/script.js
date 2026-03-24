2dedb37d604da20b8d6cf383ac2e11a3c536bef8
MeriKotevska <pupuncevska54@gmail.com>
2026-03-24 21:50:41 +0100

    Add files via upload

---
+++ JavaScript dom/script.js
@@ -0,0 +1,36 @@
+ const VonredniBtn= document.getElementById("btn")
+ const StrP= document.getElementById("StrP")
+ const WebP= document.getElementById("WebP")
+ const OOP= document.getElementById("OOP")
+ 
+ let sp = 110;
+ let vp = 50;
+ let oop = 5;
+
+ StrP.innerHTML=`Strukturno Programiranje: ${sp}`
+ WebP.innerHTML=`Web Programiranje: ${vp}`
+ OOP.innerHTML=`OOP: ${oop}`
+
+VonredniBtn.addEventListener("click",()=>{
+sp=sp-5
+vp=vp-5
+oop=oop-5
+
+if(sp>0){
+    StrP.innerHTML=`Strukturno Programiranje: ${sp}`
+}else if(sp<0||sp===0){
+    StrP.innerHTML=`Site studenti se prefrlija vonredno.`
+}
+
+if(vp>0){
+    WebP.innerHTML=`Strukturno Programiranje: ${vp}`
+}else if(vp<0||vp===0){
+    WebP.innerHTML=`Site studenti se prefrlija vonredno.`
+}
+
+if(oop>0){
+    OOP.innerHTML=`OOP: ${oop}`
+}else if(oop<0||oop===0){
+    OOP.innerHTML="OOP: Site studenti se prefrlija vonredno."
+}
+})
